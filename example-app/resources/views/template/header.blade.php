5c598d285c8fa3f3c4a8f3004d9212d4237f07dd
Fourllan <krab336@mail.ru>
2022-09-09 20:21:41 +0300

    some-changes

--- example-app/resources/views/template/header.blade.php
+++ example-app/resources/views/template/header.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.css"/>
+    <link href="resources/css/app.css" rel="stylesheet">
 </head>
 <body>
-    <div class="wrapper">
+    <div class="wrapper bg-light">
         <header class="header bg-info p-3">
             <div class="header__inner d-flex justify-content-between container align-items-center">
                 <div class="logo">
