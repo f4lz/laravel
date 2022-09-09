efe58a171f67850f2856c2ae28fafa7492bc8904
Fourllan <krab336@mail.ru>
2022-09-09 19:58:58 +0300

    added-laravel-project

---
+++ example-app/resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+    <footer class ="footer container d-flex justify-content-between">
+        <p>Igor Rogoza</p>
+        <p>211-323</p>
+    </footer>
+    <script src="/bootstrap/js/bootstrap.js"></script>
+</div>
+</body>
+</html>
---
+++ example-app/resources/views/template/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css"/>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header bg-info p-3">
+            <div class="header__inner d-flex justify-content-between container align-items-center">
+                <div class="logo">
+                    <img src="" alt="Logo">
+                </div>
+                <nav class="header__inner-nav nav">
+                    <ul class="nav__list d-flex justify-content-between align-items-center">
+                        @foreach($items as $item)
+                            <li class="ml-5 black"><a class="text-dark line-style-type-none" href="#">{{$item}}</a></li>
+                        @endforeach
+                    </ul>
+                </nav>
+            </div>
+        </header>      
+    </div>
+                            
